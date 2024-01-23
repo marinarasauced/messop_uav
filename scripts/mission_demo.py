333b7fb5b59f8640707643b8b2946c40954ec50a
Alex Ballentine <aeballentine@wpi.edu>
2024-01-23 11:34:47 -0500

    Added segment to move to a position

--- scripts/mission_demo.py
+++ scripts/mission_demo.py
@@ -1,17 +1,16 @@
 import rospy
-from mavros_msgs.srv import CommandBool, CommandTOL
-from mavros_msgs.srv import CommandBoolRequest, CommandBoolResponse, CommandTOLRequest, CommandTOLResponse
 import mavros_msgs
+import geometry.msgs
 
 
 def set_mode():
     rospy.wait_for_service('/mavros/set_mode', timeout=30)
     try:
-        data = mavros_msgs.srv.SetModeRequest()
-        data.custom_mode = 'GUIDED'
+        change_mode = mavros_msgs.srv.SetModeRequest()
+        change_mode.custom_mode = 'GUIDED'
         service_caller = rospy.ServiceProxy('/mavros/set_mode', mavros_msgs.srv.SetMode)
-        service_caller(data)
-        return 1
+        response = service_caller(change_mode)
+        return response.success
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
 
@@ -19,42 +18,72 @@ def set_mode():
 def arm():
     rospy.wait_for_service('/mavros/cmd/arming', timeout=30)
     try:
-        data = mavros_msgs.srv.CommandBoolRequest()
-        data.value = True
+        cmd_arm = mavros_msgs.srv.CommandBoolRequest()
+        cmd_arm.value = True
         service_caller = rospy.ServiceProxy('/mavros/cmd/arming', mavros_msgs.srv.CommandBool)
-        service_caller(data)
-        return 1
+        response = service_caller(cmd_arm)
+        return response.success
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
 
 
 def takeoff(altitude):
-    rospy.wait_for_service('/mavros/cmd/takeoff')
+    rospy.wait_for_service('/mavros/cmd/takeoff', timeout=30)
     try:
-        takeoff_service = rospy.ServiceProxy('/mavros/cmd/takeoff', CommandTOL)
-        takeoff_request = CommandTOLRequest(altitude=altitude, latitude=0, longitude=0, min_pitch=0, yaw=0)
-        response = takeoff_service(takeoff_request)
-        return 1
+        cmd_takeoff = mavros_msgs.srv.CommandTOLRequest()
+        takeoff_request = cmd_takeoff(altitude=altitude, latitude=0, longitude=0, min_pitch=0, yaw=0)
+        service_caller = rospy.ServiceProxy('/mavros/cmd/takeoff', mavros_msgs.srv.CommandTOL)
+        response = service_caller(takeoff_request)
+        return response.success
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
 
 
+# import rospy
+# from mavros_msgs.msg import PositionTarget
+# from geometry_msgs.msg import PoseStamped
+
+def move_to_position(x, y, z):
+    # Create a mavros PositionTarget message
+    desired_position = mavros_msgs.srv.PositionTarget()
+    desired_position.coordinate_frame = desired_position.FRAME_LOCAL_OFFSET_NED
+    desired_position.type_mask = desired_position.IGNORE_VX | desired_position.IGNORE_VY | desired_position.IGNORE_VZ | \
+                                    desired_position.IGNORE_AFX | desired_position.IGNORE_AFY | desired_position.IGNORE_AFZ | \
+                                    desired_position.FORCE | desired_position.IGNORE_YAW | desired_position.IGNORE_YAW_RATE
+
+    # Set the target position
+    desired_position.position.x = x
+    desired_position.position.y = y
+    desired_position.position.z = z
+
+    # Set the target orientation (optional)
+    desired_position.yaw = 0.0  # Set the desired yaw angle in radians
+
+    # Publish the PositionTarget message
+    position_target_pub = rospy.Publisher('/mavros/setpoint_raw/local', mavros_msgs.srv.PositionTarget, queue_size=10)
+    rate = rospy.Rate(10)  # 10 Hz
+    for _ in range(10):  # Publish for 1 second
+        position_target_pub.publish(desired_position)
+        rate.sleep()
+
+
 if __name__ == "__main__":
-    rospy.init_node('takeoff_node', anonymous=True)
+    rospy.init_node('UAV_node', anonymous=True)
 
     # Set mode to GUIDED
-    if set_mode() == 1:
+    if set_mode():
         print('Vehicle is in GUIDED mode')
         # Arm the vehicle
-        if arm() == 1:
+        if arm():
             print("Vehicle armed")
 
             # Specify the takeoff altitude (in meters)
             takeoff_altitude = 5.0
 
             # Takeoff
-            if takeoff(takeoff_altitude) == 1:
+            if takeoff(takeoff_altitude):
                 print("Vehicle taking off to {} meters".format(takeoff_altitude))
+                move_to_position(10, 4, 3)
             else:
                 print("Takeoff failed")
         else:
