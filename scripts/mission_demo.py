6e4b2e614beaefba7e61d98b8db4caa457565bf7
aeballentine <aeballentine@wpi.edu>
2024-01-25 16:36:32 +0000

    Refined VICON support

--- scripts/mission_demo.py
+++ scripts/mission_demo.py
@@ -1,17 +1,17 @@
 import rospy
 import mavros_msgs
 import geometry_msgs
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, QuaternionStamped
 from mavros_msgs.srv import CommandBool
 from vicon_integration import StateUAV
 import time
 
 
-def set_mode():
+def set_mode(mode):
     rospy.wait_for_service('/mavros/set_mode', timeout=30)
     try:
         change_mode = mavros_msgs.srv.SetModeRequest()
-        change_mode.custom_mode = 'GUIDED'
+        change_mode.custom_mode = mode
         service_caller = rospy.ServiceProxy('/mavros/set_mode', mavros_msgs.srv.SetMode)
         service_caller(change_mode)
         return True
@@ -51,28 +51,32 @@ def takeoff(altitude):
 
 def move_to_position(x, y, z):
     # Create a mavros PositionTarget message
-    desired_position = mavros_msgs.msg.PositionTarget()
-    desired_position.coordinate_frame = desired_position.FRAME_LOCAL_NED
-    desired_position.type_mask = desired_position.IGNORE_VX | desired_position.IGNORE_VY | desired_position.IGNORE_VZ | \
-                                 desired_position.IGNORE_AFX | desired_position.IGNORE_AFY | desired_position.IGNORE_AFZ | \
-                                 desired_position.FORCE | desired_position.IGNORE_YAW | desired_position.IGNORE_YAW_RATE
+    if x < local_state.x_lim & y < local_state.y_lim & z < local_state.z_lim:
+        desired_position = mavros_msgs.msg.PositionTarget()
+        desired_position.coordinate_frame = desired_position.FRAME_LOCAL_NED
+        desired_position.type_mask = desired_position.IGNORE_VX | desired_position.IGNORE_VY | desired_position.IGNORE_VZ | \
+                                     desired_position.IGNORE_AFX | desired_position.IGNORE_AFY | desired_position.IGNORE_AFZ | \
+                                     desired_position.FORCE | desired_position.IGNORE_YAW | desired_position.IGNORE_YAW_RATE
 
-    # Set the target position
-    desired_position.position.x = x
-    desired_position.position.y = y
-    desired_position.position.z = z
+        # Set the target position
+        desired_position.position.x = x
+        desired_position.position.y = y
+        desired_position.position.z = z
 
-    # Set the target orientation (optional)
-    desired_position.yaw = 0.0  # Set the desired yaw angle in radians
+        # Set the target orientation (optional)
+        desired_position.yaw = 0.0  # Set the desired yaw angle in radians
 
-    # Publish the PositionTarget message
-    position_target_pub = rospy.Publisher('/mavros/setpoint_raw/local', mavros_msgs.msg.PositionTarget, queue_size=10)
-    rate = rospy.Rate(10)  # 10 Hz
-    while ((local_state.Px - x) ** 2 + (local_state.Py - y) ** 2 + (local_state.Pz - z) ** 2) ** (0.5) > local_state.error_tol:
-        position_target_pub.publish(desired_position)
-        pass
+        # Publish the PositionTarget message
+        position_target_pub = rospy.Publisher('/mavros/setpoint_raw/local', mavros_msgs.msg.PositionTarget, queue_size=10)
+        rate = rospy.Rate(10)  # 10 Hz
+        while ((local_state.Px - x) ** 2 + (local_state.Py - y) ** 2 + (local_state.Pz - z) ** 2) ** (0.5) > local_state.error_tol:
+            position_target_pub.publish(desired_position)
+            pass
 
-    return True
+        return True
+
+    else:
+        print('Destination point is invalid')
 
 
 def land():
@@ -82,7 +86,12 @@ def land():
         land_request = cmd_land(altitude=0, latitude=0, longitude=0, min_pitch=0, yaw=0)
         service_caller = rospy.ServiceProxy('/mavros/cmd/land', mavros_msgs.srv.CommandTOL)
         response = service_caller(land_request)
+
+        while abs(local_state.Pz - 0) > local_state.error_tol:
+            pass
+
         return response.success
+
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
 
@@ -91,9 +100,11 @@ if __name__ == "__main__":
     rospy.init_node('UAV_node', anonymous=True)
 
     local_state = StateUAV()
-    print(local_state.Px)
+    local_state.x_lim = 25
+    local_state.y_lim = 25
+    local_state.z_lim = 30
     # Update the position data:
-    # local_state.x, local_state.y, and local_state.z are now available for all functions
+    # local_state.Px, local_state.Py, and local_state.Pz are now available for all functions
     rospy.wait_for_message('/mavros/local_position/pose', PoseStamped)
     print('Position data received')
     rospy.Subscriber('/mavros/local_position/pose', PoseStamped, local_state.publish_local_position)
@@ -102,10 +113,20 @@ if __name__ == "__main__":
         print('Waiting for position data')
         time.sleep(1)
 
-    print(local_state.Py)
+    local_state.set_odometry_parameters()
+
+    time.sleep(5)
+
+    # rospy.wait_for_message('/mavros/imu/data', PoseStamped)
+    # print('Position data received')
+    # rospy.Subscriber('/mavros/local_position/pose', PoseStamped, local_state.publish_local_position)
+    #
+    # while local_state.Px is None:
+    #     print('Waiting for position data')
+    #     time.sleep(1)
 
     # Set mode to GUIDED
-    if set_mode():
+    if set_mode('GUIDED'):
         print('Vehicle is in GUIDED mode')
         # Arm the vehicle
         if arm(True):
@@ -120,7 +141,7 @@ if __name__ == "__main__":
     takeoff(takeoff_altitude)
     print('Vehicle is at: ', takeoff_altitude, ' m')
 
-    move_to_position(25, 30, 20)
+    move_to_position(24, 30, 20)
     print("UAV has reached waypoint")
 
     # Land
@@ -129,10 +150,7 @@ if __name__ == "__main__":
     else:
         print("Land failed")
 
-    # Wait to allow land command to finish
-    rospy.sleep(15)
-
-    # Disarm the motors is not already done
+    # Disarm the motors
     if arm(False):
         print('Vehicle disarmed')
     else:
--- scripts/vicon_integration.py
+++ scripts/vicon_integration.py
@@ -1,38 +1,49 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
+from mavros_msgs.srv import ParamSet
 
 
 class StateUAV:
     def __init__(self):
+        # The codes knowledge of where the UAV is
         self.Px = None
         self.Py = None
         self.Pz = None
-        self.error_tol = 0.2    # in meters
+
+        # Basic components: publisher and error tolerance
+        # Publisher is used to update the position of the UAV
+        # Error tolerance is used to monitor when the vehicle has reached its destination
+        # Time_vicon is the measurement of when the vicon data was last received
+        # Time_odometry
+        self.pub = rospy.Publisher('/mavros/local_position/pose', PoseStamped, queue_size=10)
+        self.error_tol = 0.1    # in meters
+        self.time_vicon = None
+
+        # limits of where the UAV can fly:
+        self.x_lim = None
+        self.y_lim = None
+        self.z_lim = None
+
+    @staticmethod
+    def set_odometry_parameters():
+        rospy.wait_for_service('/mavros/param/set')
+        param_set_service = rospy.ServiceProxy('/mavros/param/set', ParamSet)
+
+        # set the priority for /mavros/odometry/out
+        param_set_service(param_id='EK3_SRC1_POSZ', value=[1, 0.0])
+
+        # set the priority for /mavros/odometry/in
+        param_set_service(param_id='EK3_SRC2_POSZ', value=[1, 1.0])
 
     def publish_local_position(self, position):
-        # rospy.init_node('local_position_publisher', anonymous=True)
-        pub = rospy.Publisher('/mavros/local_position/pose', PoseStamped, queue_size=10)
-
-        # rate = rospy.Rate(10)  # Use this to dictate the frequency that location is published
-
         # Constantly update the location of the UAV
-        # while not rospy.is_shutdown():
-        # Create a PoseStamped message
-        local_pose = PoseStamped()
-        local_pose.header.stamp = rospy.Time.now()
-        local_pose.header.frame_id = 'map'  # Indicate that coordinates are global
 
-        # Set local position
-        local_pose.pose.position.x = position.pose.position.x
-        local_pose.pose.position.y = position.pose.position.y
-        local_pose.pose.position.z = position.pose.position.z
-
-        # Publish to mavros
-        pub.publish(position)
+        # Publish the position to mavros
+        self.pub.publish(position)
 
         # update the local position variable:
         self.Px = position.pose.position.x      # local_pose.pose.position.x
         self.Py = position.pose.position.y      # local_pose.pose.position.y
         self.Pz = position.pose.position.z      # local_pose.pose.position.z
 
-            # rate.sleep()
+
